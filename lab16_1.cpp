113e30bf526c5a1707ec0e9690830fc112569aaa
worapit <drm.wmwork@gmail.com>
2022-01-14 15:36:04 +0700

    complete

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -1,9 +1,27 @@
-include <iostream>
+#include<iostream>
 using namespace std;
 
 int main(){
+    int a =5;
+    string b = "A" ;
+    string &c = b;
+    int *x = &a;
+    string *y = &b;
+    int **z = &x;
+    cout << a << " " << b << " " << c << " " << x << " "  << y << " " << z << "\n";
+	cout << &a << " " << &b << " " << &c << " " << &x << " "  << &y << " " << &z << "\n";
 
-	
+	c = "F";
+	cout << a << " " << b << " " << c << " " << x << " "  << y << " " << z << "\n";
+
+	*y = "W";
+    cout << a << " " << b << " " << c << " " << x << " "  << y << " " << z << "\n";
+
+	*x = 6;
+	cout << a << " " << b << " " << c << " " << x << " "  << y << " " << z << "\n";
+
+	**z = 7;
+	cout << a << " " << b << " " << c << " " << x << " "  << y << " " << z << "\n";
 	
 	return 0;
 }
